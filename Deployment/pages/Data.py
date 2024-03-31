91643797ea2d98dda4b8787ecfecd29f19ddc704
Mohamed Talaat <147813555+mohamedtalaat91@users.noreply.github.com>
2024-03-31 15:03:28 +0200

    Update Data.py

--- Deployment/pages/Data.py
+++ Deployment/pages/Data.py
@@ -16,21 +16,4 @@ st.markdown('''<center> <h1> US Accidents Data </h1> </center>''',unsafe_allow_h
 st.markdown('<a href ="https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents/data"> <center> <h2> Link </h2> </center> ', unsafe_allow_html=True)
 st.write(data)
 
-st.markdown('''<center> <h1> Used data after cleaning and feature engineering</h1> </center>''',unsafe_allow_html=True)
-zip_file_path = "./Deployment/source/US_data_cleand.zip"
-with ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall()
-extracted_files = zip_ref.namelist()
-csv_file_name = extracted_files[0] 
-data_cleand = pd.read_csv(csv_file_name)
-st.write(data_cleand)
-
-st.markdown('''<center> <h1>Accident Conditions Data</h1> </center>''',unsafe_allow_html=True)
-zip_file_path = "./Deployment/source/accidents_conditions.zip"
-with ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall()
-extracted_files = zip_ref.namelist()
-csv_file_name = extracted_files[0] 
-accidents_conditions = pd.read_csv(csv_file_name)
-st.write(accidents_conditions)
 
