81272c2c7958fc71252520cc357734b7ee6f239c
Mohamed Talaat <147813555+mohamedtalaat91@users.noreply.github.com>
2024-03-31 17:18:16 +0200

    Add files via upload

---
+++ Deployment/pages/Data.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import streamlit as st 
+from zipfile import ZipFile
+
+
+
+
+zip_file_path = r"E:\Epsilon AI\MID PROJECT\Deployment\source\US_Accidents_March23_analysis.zip"
+with ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall()
+extracted_files = zip_ref.namelist()
+csv_file_name = extracted_files[0] 
+data = pd.read_csv(csv_file_name)
+
+st.markdown('''<center> <h1> US Accidents Data </h1> </center>''',unsafe_allow_html=True)
+st.markdown('<a href ="https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents/data"> <center> <h2> Link </h2> </center> ', unsafe_allow_html=True)
+st.write(data)
+
